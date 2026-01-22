b784216b5446abfbff556628bac946ba13f5f1ec
calluna-lt3 <106458302+calluna-lt3@users.noreply.github.com>
2026-01-22 10:40:36 -0800

    remove root password

--- modules/nixos/users.nix
+++ modules/nixos/users.nix
@@ -6,7 +6,7 @@
     users.mutableUsers = false;
     users.users = {
         root = {
-            hashedPassword = "$y$j9T$xMOfxBXWwSXKO.Mw6XFky0$uiPwFFK5x9S0JSQ9mT7neYHM7lSI1M4E86bWzgnBFsB";
+            hashedPassword = "*";
         };
         "mlwpsh" = {
             isNormalUser = true;
